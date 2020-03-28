40a5d29ec2e56b65535567e7cb55f6c581662349
Daniel Wei <dominum15@outlook.com>
2020-03-28 17:01:35 -0500

    Update admin wrapper
    Should properly ask for admin rights now

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 import os
+import ctypes, sys
+import runpy
+
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
 if __name__ == '__main__':
-    os.system("runas /user:Administrator \"python main.py\"")
+    if is_admin():
+        runpy.run_path('./main.py', run_name='__main__')
+    else:
+        # Re-run the program with admin rights
+        ctypes.windll.shell32.ShellExecuteW(None, u"runas", unicode(sys.executable), unicode(" ".join(sys.argv[1:])), None, 1)
