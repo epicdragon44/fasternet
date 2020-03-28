0cdb405c71bca1fe018cdd7ef1421ac65bd13832
Daniel Wei <dominum15@outlook.com>
2020-03-28 15:54:04 -0500

    Add admin privileges

--- main.py
+++ main.py
@@ -1,81 +1,92 @@
 # coding=utf-8
 import runpy
-import sys
 import os
 import codecs
+import ctypes, sys
 from os.path import expanduser
-import subprocess
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
 
 if __name__ == '__main__':
-    # HERE BEGINS THE SECTION WHERE WE RUN NAMEBENCH
+    if is_admin():
+        # HERE BEGINS THE SECTION WHERE WE RUN NAMEBENCH
 
-    sys.argv = ['', '-x']
-    runpy.run_path('./namebench.py', run_name='__main__')
+        sys.argv = ['', '-x']
+        runpy.run_path('./namebench.py', run_name='__main__')
 
-    # HERE BEGINS THE SECTION WHERE WE PARSE THE HTML TO GET THE ADDRESSES
+        # HERE BEGINS THE SECTION WHERE WE PARSE THE HTML TO GET THE ADDRESSES
 
-    # cd into user home directory
-    home = expanduser("~")
-    # find all namebench files
-    fileList = []
-    path = home + '\\AppData\\Local\\Temp'
-    keyword = 'namebench'
+        # cd into user home directory
+        home = expanduser("~")
+        # find all namebench files
+        fileList = []
+        path = home + '\\AppData\\Local\\Temp'
+        keyword = 'namebench'
 
-    for fname in os.listdir(path):
-        if fname.endswith('.html'):
-            if keyword in fname:
-                fileList.append(fname)
+        for fname in os.listdir(path):
+            if fname.endswith('.html'):
+                if keyword in fname:
+                    fileList.append(fname)
 
-    # take latest file so that the newest run is used
-    fileList.sort()
-    fileUsed = path + "\\" + fileList[-1]
+        # take latest file so that the newest run is used
+        fileList.sort()
+        fileUsed = path + "\\" + fileList[-1]
 
-    # line with all the server names and IP
-    primaryServer = ""
-    secondServer = ""
-    thirdServer = ""
+        # line with all the server names and IP
+        primaryServer = ""
+        secondServer = ""
+        thirdServer = ""
 
-    f = codecs.open(fileUsed, 'r')
-    lines = f.readlines()
+        f = codecs.open(fileUsed, 'r')
+        lines = f.readlines()
 
-    # reads file and finds 1st, 2nd
-    for i in range(len(lines)):
-        if '<td>Primary Server</td>' in lines[i]:
-            primaryServer = lines[i + 1]
-        if '<td>Secondary Server</td>' in lines[i]:
-            secondServer = lines[i + 1]
+        # reads file and finds 1st, 2nd
+        for i in range(len(lines)):
+            if '<td>Primary Server</td>' in lines[i]:
+                primaryServer = lines[i + 1]
+            if '<td>Secondary Server</td>' in lines[i]:
+                secondServer = lines[i + 1]
 
-    # parses substrings for IP address only
-    primaryServerIP = ""
-    index1 = primaryServer.find('ip') + 4
-    primaryServerIP = primaryServer[index1:]
-    index2 = primaryServerIP.find('</div>')
-    primaryServerIP = primaryServerIP[:index2]
+        # parses substrings for IP address only
+        primaryServerIP = ""
+        index1 = primaryServer.find('ip') + 4
+        primaryServerIP = primaryServer[index1:]
+        index2 = primaryServerIP.find('</div>')
+        primaryServerIP = primaryServerIP[:index2]
 
-    secondServerIP = ""
-    index1 = secondServer.find('ip') + 4
-    secondServerIP = secondServer[index1:]
-    index2 = secondServerIP.find('</div>')
-    secondServerIP = secondServerIP[:index2]
+        secondServerIP = ""
+        index1 = secondServer.find('ip') + 4
+        secondServerIP = secondServer[index1:]
+        index2 = secondServerIP.find('</div>')
+        secondServerIP = secondServerIP[:index2]
 
-    # PLEASE READ: primaryServerIP and secondaryServerIP are the IP addresses only
-    print (primaryServerIP)
-    print (secondServerIP)
+        # PLEASE READ: primaryServerIP and secondaryServerIP are the IP addresses only
+        print (primaryServerIP)
+        print (secondServerIP)
 
-    # HERE BEGINS THE SECTION WHERE WE IMPLEMENT THE NEW DNS SERVERS USING POWERSHELL
+        # HERE BEGINS THE SECTION WHERE WE IMPLEMENT THE NEW DNS SERVERS USING POWERSHELL
 
-    # Ethernet
-    cmdCommand1 = "Set-DNSClientServerAddress \"Ethernet\" –ServerAddresses (\""
-    cmdCommand1+=primaryServerIP
-    cmdCommand1+="\", \""
-    cmdCommand1+=secondServerIP
-    cmdCommand1+="\")"
-    os.system(cmdCommand1)
+        # Ethernet
+        cmdCommand1 = "powershell.exe Set-DNSClientServerAddress \"Ethernet\" –ServerAddresses (\""
+        cmdCommand1 += primaryServerIP
+        cmdCommand1 += "\", \""
+        cmdCommand1 += secondServerIP
+        cmdCommand1 += "\")"
+        os.system(cmdCommand1)
 
-    #Wi-Fi
-    cmdCommand2 = "Set-DNSClientServerAddress \"Wi-Fi\" –ServerAddresses (\""
-    cmdCommand2+= primaryServerIP
-    cmdCommand2 += "\", \""
-    cmdCommand2 += secondServerIP
-    cmdCommand2 += "\")"
-    os.system(cmdCommand2)
+        # Wi-Fi
+        cmdCommand2 = "powershell.exe Set-DNSClientServerAddress \"Wi-Fi\" –ServerAddresses (\""
+        cmdCommand2 += primaryServerIP
+        cmdCommand2 += "\", \""
+        cmdCommand2 += secondServerIP
+        cmdCommand2 += "\")"
+        os.system(cmdCommand2)
+    else:
+        # Re-run the program with admin rights
+        ctypes.windll.shell32.ShellExecuteW(None, u"runas", unicode(sys.executable), unicode(" ".join(sys.argv[1:])),
+                                            None, 1)
